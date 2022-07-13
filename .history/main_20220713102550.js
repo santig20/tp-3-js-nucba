3ae280d2e9237defe3016d32114a175295381ed0
santig20 <santiagogrenat@gmail.com>
2022-07-13 12:17:06 -0300

    first commit

---
+++ .history/main_20220713102550.js
@@ -0,0 +1,92 @@
+const inputNum = document.querySelector(".inputSearch");
+const inputBTN = document.querySelector(".inputBtn");
+const menu = document.querySelector(".btn__container");
+const item = document.querySelectorAll(".item");
+const cartList = document.querySelector(".cartContainer");
+const catalogo = document.querySelector(".catalogo");
+const header = document.querySelector(".header");
+const cartCont = document.querySelector(".carrito__Container");
+const btnCompra = document.querySelectorAll(".btnCatalogo");
+const totalItems = document.querySelector("#totalItems");
+const totalProducts = document.querySelector("#totalProducts");
+const borrarItemCart = document.querySelectorAll(".borrarCart");
+
+let pizza1 =  [
+     { id:1 ,nombre: "especial",ingredientes:["queso","jamon","morron","huevo"], precio:450},
+ {  id :2 ,nombre: "4 quesos",ingredientes:["queso","roquefort","parmesano","muzzarela"], precio:550},
+ { id:3 , nombre:"pollo",ingredientes:["queso","jamon","pollo","huevo"], precio:600},
+{ id:4 ,nombre: "camtimpalo",ingredientes:["queso","camtimpalo",],precio:500},
+ { id: 5 , nombre:"americana",ingredientes:["queso","chedar","panceta",],precio:650},
+{  id:6 , nombre:"caprese",ingredientes:["queso","tomate","albaca",],precio:400} ,
+];
+let ultimoScrollTop;
+window.addEventListener("scroll", () => {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > ultimoScrollTop) {
+    header.style.top = "-110px";
+  } else {
+    header.style.top = "0";
+  }
+
+  ultimoScrollTop = scrollTop;
+});
+
+menu.addEventListener("click", () => {
+  item.forEach((i) => i.classList.toggle("show"));
+});
+
+inputBTN.addEventListener("click", buscarPizza);
+
+function buscarPizza(e) {
+  e.preventDefault();
+  const pizzaID = inputNum.value;
+  if (pizzaID === "") {
+    showError("Por favor ingresa el ID de la pizza !");
+    return;
+  }
+  createHTML();
+  inputNum.value = "";
+}
+
+function showError(error) {
+  const msgError = document.createElement("p");
+  msgError.textContent = error;
+  msgError.classList.add("error");
+  cartList.appendChild(msgError);
+  setTimeout(() => {
+    msgError.remove();
+  }, 2000);
+}
+
+function createHTML() {
+  cartList.innerHTML = "";
+  if (pizzas.some((pizza) => pizza.id == inputNum.value)) {
+    pizzas.forEach((pizza) => {
+      if (pizza.id === inputNum.valueAsNumber) {
+        const h2 = document.createElement("h2");
+        const h3 = document.createElement("h3");
+        const h4 = document.createElement("h4");
+        const span = document.createElement("span");
+        cartList.classList.add("cartList2");
+        h2.innerHTML = `Esa es la ${pizza.nombre}`;
+        h3.innerHTML = `Ingredientes: ${pizza.ingrdientes}`;
+        h4.innerHTML = `$ ${pizza.precio}`;
+        span.innerHTML = `X`;
+        cartList.appendChild(h2);
+        cartList.appendChild(h3);
+        cartList.appendChild(h4);
+        cartList.appendChild(span);
+        span.classList.add("borrar");
+        span.addEventListener("click", (e) => {
+          const item = e.target.parentElement;
+          cartList.innerHTML = "";
+        });
+      } else {
+        return;
+      }
+    });
+  } else {
+    showError("No hay ninguna pizza listada con ese ID!");
+    return;
+  }
+}
---
+++ .history/main_20220713102744.js
@@ -0,0 +1,144 @@
+const inputNum = document.querySelector(".inputSearch");
+const inputBTN = document.querySelector(".inputBtn");
+const menu = document.querySelector(".btn__container");
+const item = document.querySelectorAll(".item");
+const cartList = document.querySelector(".cartContainer");
+const catalogo = document.querySelector(".catalogo");
+const header = document.querySelector(".header");
+const cartCont = document.querySelector(".carrito__Container");
+const btnCompra = document.querySelectorAll(".btnCatalogo");
+const totalItems = document.querySelector("#totalItems");
+const totalProducts = document.querySelector("#totalProducts");
+const borrarItemCart = document.querySelectorAll(".borrarCart");
+let pizzas = [
+  {
+    id: 1,
+    nombre: "especial",
+    ingrdientes: [
+      "huevo",
+      "muzzarella",
+      "tomate",
+      "morron",
+      "aceitunas verdes",
+    ],
+    precio: 1200,
+    img: "./imagenes/napolitana.jpg",
+  },
+  {
+    id: 2,
+    nombre: "Polemica",
+    ingrdientes: ["salsa de tomate", "muzzarella", "anana", "aceitunas negras"],
+    precio: 1300,
+    img: "./imagenes/muzza.jpg",
+  },
+  {
+    id: 3,
+    nombre: "Muzza",
+    ingrdientes: ["salsa de tomate", "muzzarella", "aceitunas verdes"],
+    precio: 500,
+    img: "./imagenes/morrones.jpg",
+  },
+  {
+    id: 4,
+    nombre: "de morrones",
+    ingrdientes: ["salsa de tomate", "muzzarella", "morrones", "ajo"],
+    precio: 1200,
+    img: "./imagenes/morrones.jpg",
+  },
+  {
+    id: 5,
+    nombre: "cuatro quesos",
+    ingrdientes: [
+      "salsa de tomate",
+      "muzzarella",
+      "provolone",
+      "parmesano",
+      "fontina",
+    ],
+    precio: 1400,
+    img: "./imagenes/cuatroquesos.jpg",
+  },
+  {
+    id: 6,
+    nombre: "Fugazzeta",
+    ingrdientes: [
+      "salsa de tomate",
+      "muzzarella",
+      "cebolla",
+      "aceitunas verdes",
+    ],
+    precio: 1200,
+    img: "./imagenes/fugazeta.jpg",
+  },
+];
+let ultimoScrollTop;
+window.addEventListener("scroll", () => {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > ultimoScrollTop) {
+    header.style.top = "-110px";
+  } else {
+    header.style.top = "0";
+  }
+
+  ultimoScrollTop = scrollTop;
+});
+
+menu.addEventListener("click", () => {
+  item.forEach((i) => i.classList.toggle("show"));
+});
+
+inputBTN.addEventListener("click", buscarPizza);
+
+function buscarPizza(e) {
+  e.preventDefault();
+  const pizzaID = inputNum.value;
+  if (pizzaID === "") {
+    showError("Por favor ingresa el ID de la pizza !");
+    return;
+  }
+  createHTML();
+  inputNum.value = "";
+}
+
+function showError(error) {
+  const msgError = document.createElement("p");
+  msgError.textContent = error;
+  msgError.classList.add("error");
+  cartList.appendChild(msgError);
+  setTimeout(() => {
+    msgError.remove();
+  }, 2000);
+}
+
+function createHTML() {
+  cartList.innerHTML = "";
+  if (pizzas.some((pizza) => pizza.id == inputNum.value)) {
+    pizzas.forEach((pizza) => {
+      if (pizza.id === inputNum.valueAsNumber) {
+        const h2 = document.createElement("h2");
+        const h3 = document.createElement("h3");
+        const h4 = document.createElement("h4");
+        const span = document.createElement("span");
+        cartList.classList.add("cartList2");
+        h2.innerHTML = `Esa es la ${pizza.nombre}`;
+        h3.innerHTML = `Ingredientes: ${pizza.ingrdientes}`;
+        h4.innerHTML = `$ ${pizza.precio}`;
+        span.innerHTML = `X`;
+        cartList.appendChild(h2);
+        cartList.appendChild(h3);
+        cartList.appendChild(h4);
+        cartList.appendChild(span);
+        span.classList.add("borrar");
+        span.addEventListener("click", (e) => {
+          const item = e.target.parentElement;
+          cartList.innerHTML = "";
+        });
+      } else {
+        return;
+      }
+    });
+  } else {
+    showError("No hay ninguna pizza listada con ese ID!");
+    return;
+  }
+}
